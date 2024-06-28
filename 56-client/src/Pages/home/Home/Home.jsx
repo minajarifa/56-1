0356295eed91b36be0b1c23d4eaaec3ea79c71f3
Arifa Islam <minajmiha02@gmail.com>
2024-06-28 15:56:10 +0600

    first commit

---
+++ 56-client/src/Pages/home/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Banner from "../Banner/Banner";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Banner/>
+            <h1 className="text-9xl">Home</h1>
+        </div>
+    );
+};
+
+export default Home;
